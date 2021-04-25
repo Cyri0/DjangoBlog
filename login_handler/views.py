9f850e7dfad456b627b252e0769b1a3a818d54ef
Cyri0 <nemestamas94@gmail.com>
2021-04-25 10:59:31 +0200

    registration works

--- login_handler/views.py
+++ login_handler/views.py
@@ -1,13 +1,23 @@
 from django.shortcuts import render
-
-def homePage(request):
-    context={}
-    return render(request, 'base.html', context)
-
-def registerPage(request):
-    context = {}
-    return render(request, 'register.html', context)
+from django.contrib.auth.forms import UserCreationForm
 
 def loginPage(request):
     context = {}
     return render(request, 'login.html', context)
+
+def registerPage(request):
+    form = UserCreationForm()
+    context = {
+        'form':form
+    }
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+    return render(request, 'register.html', context)
+
+def homePage(request):
+    context={}
+    return render(request, 'base.html', context)
